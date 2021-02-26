1261e8b081a8e9b8d8d7ec97f130d6b57db1089d
Ryan <ryan@Ryans-iMac.local>
2021-02-26 15:00:16 -0800

    deploying

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
@@ -27,4 +32,4 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
 
-//add
+//addgit 
