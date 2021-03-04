f64171b1c5b79b19aceba1615993878aaaf1382e
Ryan <ryan@Ryans-iMac.local>
2021-03-04 14:20:14 -0800

    updated port var

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config()
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -15,15 +16,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+   mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/OfflineFinance',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
 
 // routes
 app.use(require("./routes/api.js"));
